0b7415b616529c5f1676c9887671efdaf5fe0955
Siwapun Siwaporn <map.siwapun@gmail.com>
2018-07-01 05:52:00 +0700

    ifElse and inc (#13)

--- src/logic.php
+++ src/logic.php
@@ -10,3 +10,26 @@ function defaultTo()
   $curried = curryN($defaultTo, 2);
   return call_user_func_array($curried, $arguments);
 }
+
+/**
+ * (*… → Boolean) → (*… → *) → (*… → *) → (*… → *)
+ * @param callable $condition
+ * @param callable $onTrue
+ * @param callable $onFalse
+ * @return \Closure
+ */
+function ifElse()
+{
+  $ifElse = function (callable $condition, callable $onTrue, callable $onFalse) {
+    return function () use ($condition, $onTrue, $onFalse) {
+      $arguments = func_get_args();
+      if (call_user_func_array($condition, $arguments)) {
+        return call_user_func_array($onTrue, $arguments);
+      }
+      return call_user_func_array($onFalse, $arguments);
+    };
+  };
+  $arguments = func_get_args();
+  $curried = curryN($ifElse, 3);
+  return call_user_func_array($curried, $arguments);
+}
