783b42837600778791b93d5da346a8a500ea9ccb
siwapunsiwaporn <siwapun.si@carmana.com>
2018-07-01 05:25:18 +0700

    fix groupBy test case

--- test/GroupByTest.php
+++ test/GroupByTest.php
@@ -9,11 +9,11 @@ class GroupByTest extends TestCase
   public function testGroupByIsEven()
   {
     $isEven = function ($value) {
-      return $value % 2 === 0;
+      return $value % 2 === 0 ? 'even' : 'odd';
     };
     $expected = [
-      true => [2, 4, 6],
-      false => [1, 3, 5]
+      'even' => [2, 4, 6],
+      'odd' => [1, 3, 5]
     ];
     $actual = groupBy($isEven)([1, 2, 3, 4, 5, 6]);
     $this->assertEquals($expected, $actual);
